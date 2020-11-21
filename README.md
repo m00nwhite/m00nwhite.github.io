8369d00eccd4ffb508cd81e3d9c17e457b8cf34d
yiny <174821300@qq.com>
2020-11-21 20:01:16 +0800

    dt

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # github.io
-  独立开发者，全栈工程师，特许金融分析师。
-
-  专注IT技术、金融科技、前端设计和网络安全。
-
-  Independent developer, fullstack developer, CFA.
-
-  focus on IT and financial technology, front-end design and network security.
+  A persional blog.
---
+++ check.sh
@@ -0,0 +1,9 @@
+#########################################################
+# filename : check.sh
+# author   : moonwhite
+# version  : v1.0.0
+# function : check jekyll service. 
+#########################################################
+
+ps -ef|grep jekyll
+
